0b3174bdea3ba5a1361c1401782ced229feb819f
nishanth <dhanpalbharath456@gmail.com>
2019-04-18 00:09:40 +0800

    styles have been modified

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -3,7 +3,8 @@ import React from 'react';
 export class ProgressBar extends React.Component {
   render() {
     let style={"width":parseInt((this.props.data/this.props.limit)*100,10)+'%'};
-	let applyClass=(this.props.data>this.props.limit?"progress-status red":"progress-status");
+	let applyClass=(this.props.data>this.props.limit?"progress-status excess-limit":"progress-status");
+	
     return (
                     <div className="container">
 					    <div className="progress-bar-container">
