52a5bebdefc38601ff8714ea6db082f4c2dfb72a
nishanth <dhanpalbharath456@gmail.com>
2019-04-17 23:45:18 +0800

    added new test suites

--- src/components/__tests__/buttons.js
+++ src/components/__tests__/buttons.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount,shallow } from 'enzyme';
 import expect from 'expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -10,7 +10,26 @@ import {Button} from '../button';
 
 describe('<Button />', () => {
   it('calls render', () => {
-    const wrapper = mount(<Button />);
+    const mockLoginfn = jest.fn();
+	const value="-30";
+    const wrapper = shallow(<Button  value={value} handleSubmit={mockLoginfn}/>);
     expect(wrapper).toBeDefined();
   });
+  
+  it('find input', () => {
+    const mockLoginfn = jest.fn();
+	const value="-30";
+    const wrapper = shallow(<Button  value={value} handleSubmit={mockLoginfn}/>);
+	wrapper.find('input').simulate('click',{ target: {value:('2018-01-22')} });
+    expect(wrapper.find('input').length).toBe(1);
+  });
+  
+  it('testing prop value', () => {
+    const mockLoginfn = jest.fn();
+	const value="-30";
+    const wrapper = shallow(<Button  value={value} handleSubmit={mockLoginfn}/>);
+	expect(wrapper.props().value).toBe(value);
+  });
+  
+  
 })
--- src/components/__tests__/progressBar.js
+++ src/components/__tests__/progressBar.js
@@ -9,11 +9,17 @@ import expect from 'expect';
 import {ProgressBar} from '../progressBar';
 
 describe('<ProgressBar />', () => {
-  it('calls render', () => {
   const index=0;
-  const data=10;
-  const limit=100;
-    const wrapper = mount(<ProgressBar key={0} data={"10"} limit={"100"}/>);
+  const data="10";
+  const limit="100";
+  
+  it('calls render', () => {
+    const wrapper = mount(<ProgressBar key={index} data={data} limit={limit}/>);
     expect(wrapper).toBeDefined();
+  });
+  
+  it('content value', () => {
+    const wrapper = mount(<ProgressBar key={index} data={data} limit={limit}/>);
+    expect(wrapper.text()).toBe('10%');
   });
 })
